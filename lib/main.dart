1a5bde65a0a1d0adb1573d13317606bb5a557f92
Taha Zaryab <tahazaryab10@gmail.com>
2024-01-23 14:15:00 -0500

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media/responsive/mobile_screen_layout.dart';
 import 'package:social_media/responsive/responsive_layout_screen.dart';
 import 'package:social_media/responsive/web_screen_layout.dart';
 import 'package:social_media/utils/colours.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await dotenv.load(fileName: ".env");
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: dotenv.get('API_KEY'),
+            appId: dotenv.get('APP_ID'),
+            messagingSenderId: dotenv.get('MESSAGING_SENDER_ID'),
+            projectId: dotenv.get('PROJECT_ID'),
+            storageBucket: dotenv.get('STORAGE_BUCKET')));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
