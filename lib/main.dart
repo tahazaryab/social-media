fd6657af9dc49548f443ac4b6045a8658ddc0b01
Taha Zaryab <tahazaryab10@gmail.com>
2024-01-23 20:38:22 -0500

    Create login page ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media/pages/login_page.dart';
 import 'package:social_media/responsive/mobile_screen_layout.dart';
 import 'package:social_media/responsive/responsive_layout_screen.dart';
 import 'package:social_media/responsive/web_screen_layout.dart';
@@ -34,10 +35,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
       title: 'Flutter Demo',
+      home: LoginPage(),
+      /*
       home: const ResponsiveLayout(
         mobileScreenLayout: MobileScreenLayout(),
         webScreenLayout: WebScreenLayout(),
-      ),
+      ),*/
     );
   }
 }
